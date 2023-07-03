a5afdfa64a6762f774debc5141af301bf56d8e0d
andresgaleo <andresgaleo@gmail.com>
2023-07-03 10:29:04 -0500

    add unit test

---
+++ FlightTest/FlightControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace FlightTest
+{
+    public class FlightControllerTest : TestBuider
+    {
+        [Fact]
+        public void ConsultarVueloExito()
+        {
+            var vuelo = new
+            {
+                origen= "MZL",
+                destino="BCN"
+            };
+            var carga = this.Test.GetAsync("https://localhost:7059/Flights/find/one?Destination="+vuelo.destino+"&Origin="+vuelo.origen).Result;
+            carga.EnsureSuccessStatusCode();
+            Assert.True(true, "Rutas Consultadas Correctamente.");
+        }
+        [Fact]
+        public void ConsultarVueloError()
+        {
+            var vuelo = new
+            {
+                origen = "MED",
+                destino = "BCN"
+            };
+            var carga = this.Test.GetAsync("https://localhost:7059/Flights/find/one?Destination=" + vuelo.destino + "&Origin=" + vuelo.origen).Result;
+            carga.EnsureSuccessStatusCode();
+            Assert.True(false, "Ruta no existe.");
+        }
+        [Fact]
+        public void ConvertirValorxMoneda()
+        {
+            string monedaNueva = "COP";
+            double valorConvertir = 200;
+            var carga = this.Test.GetAsync("https://localhost:7059/Flights/currency/USD/"+monedaNueva+"/"+valorConvertir).Result;
+            carga.EnsureSuccessStatusCode();
+            Assert.True(true, "Conversion hecha correctamente.s");
+        }
+    }
+}
---
+++ FlightTest/TestBuilder.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightTest
+{
+    public abstract class TestBuider : IDisposable
+    {
+        protected HttpClient Test;
+        private bool Disposed;
+        protected TestBuider()
+        {
+            BootstrapTestingSuite();
+        }
+        protected void BootstrapTestingSuite()
+        {
+            Disposed = false;
+            var appFactory = new WebApplicationFactory<Program>();
+            Test = appFactory.CreateClient();
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public virtual void Dispose(bool disposing)
+        {
+            if (Disposed)
+                return;
+            if (disposing)
+            {
+                Test.Dispose();
+            }
+            Disposed = true;
+        }
+
+    }
+}
